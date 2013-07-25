6942876122100b52ba3c7fc57697a4389c3560f5
Matthew Fernandez <matthew.fernandez@gmail.com>
2013-07-25 17:59:00 +1000

    Implement RSS feeder properly.

--- src/feeders/rss.py
+++ src/feeders/rss.py
@@ -1,6 +1,15 @@
 import base
+import feedparser
 
 class Feeder(base.Feeder):
     def __iter__(self):
-        yield 'hello'
-        yield 'world'
+        for n, i in self.feeds.items():
+            assert 'url' in i
+            url = i['url']
+            seen = self.resource.get(url, set())
+            new = feedparser.parse(url)
+            for e in new.entries:
+                if e.id not in seen:
+                    yield (n, e.title, e.description)
+                    seen.add(e.id)
+            self.resource[url] = seen
