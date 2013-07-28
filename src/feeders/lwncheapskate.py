e6e99eca8163a721994c1a446d09a9848847525f
Matthew Fernandez <matthew.fernandez@gmail.com>
2013-07-28 11:52:47 +1000

    Add a feeder to cope with LWN's subscription model.

---
+++ src/feeders/lwncheapskate.py
@@ -0,0 +1,28 @@
+'''
+Feeder for LWN that only notifies you when a page is available to
+non-subscribers.
+'''
+
+import base, rsscommon
+import urllib2
+
+class Feeder(base.Feeder):
+    def __iter__(self):
+        for n, i in self.feeds.items():
+            assert 'url' in i
+            url = i['url']
+            seen = self.resource.get(url, set())
+            entries = rsscommon.get_entries(url)
+            for e in entries:
+                id = rsscommon.get_id(e)
+                if id not in seen:
+                    try:
+                        # See if the page is available.
+                        urllib2.urlopen(e.link)
+                        # No 503 :)
+                        yield base.Entry(n, e.title, e.description, html=True)
+                        seen.add(id)
+                    except:
+                        # 503 :(
+                        pass
+            self.resource[url] = seen
