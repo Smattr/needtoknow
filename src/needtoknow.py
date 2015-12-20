8ec6a5b5f76e28db4725d71f7d487e746059f94e
Matthew Fernandez <matthew.fernandez@gmail.com>
2015-12-20 16:40:32 +1100

    Provide progress output via logging if we have a TTY.
    Closes Bitbucket #6

--- src/needtoknow.py
+++ src/needtoknow.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-import argparse, os, sys
+import argparse, logging, os, sys
 import config, pickler, sender
 
 def get_resource_path(name):
     return os.path.join(os.path.expanduser('~'), '.needtoknow/cache/%s.pickle' % name)
 
 _PATH_APPENDED = False
-def construct_feeder(name):
+def construct_feeder(name, log):
     global _PATH_APPENDED
     if not _PATH_APPENDED:
         sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), 'feeders'))
@@ -16,7 +16,7 @@ def construct_feeder(name):
     try:
         mod = __import__(name)
     except Exception as e:
-        print >>sys.stderr, 'Warning: failed to import feeder %s: %s' % (name, e)
+        log.warning('failed to import feeder %s: %s' % (name, e))
         return None
 
     respath = get_resource_path(name)
@@ -39,27 +39,37 @@ def main():
         help='include a particular feed by name')
     opts = parser.parse_args()
 
+    log = logging.getLogger('needtoknow')
+    log.addHandler(logging.StreamHandler(sys.stderr))
+    # Determine whether we are running interactively.
+    if sys.stdout.isatty():
+        log.setLevel(logging.INFO)
+    else:
+        log.setLevel(logging.WARNING)
+
     try:
         conf = config.get_config()
     except Exception as e:
-        print >>sys.stderr, 'Failed to parse config: %s' % e
+        log.error('Failed to parse config: %s' % e)
         return -1
 
     try:
         feeds = config.get_feeds()
     except Exception as e:
-        print >>sys.stderr, 'Failed to parse feeds: %s' % e
+        log.error('Failed to parse feeds: %s' % e)
         return -1
 
     feeders = {}
 
+    log.info('Loading feeders...')
     for s in feeds.sections():
         f = feeds.get(s, 'feeder')
         if f not in feeders:
-            feeders[f] = construct_feeder(f)
+            log.info(' Loading %s...' % f)
+            feeders[f] = construct_feeder(f, log)
 
         if feeders[f] is None:
-            print >>sys.stderr, 'Warning: No feeder named %s (referenced by %s).' % (f, s)
+            log.warning(' Warning: No feeder named %s (referenced by %s).' % (f, s))
         else:
             if s in opts.include or (opts.include == [] and s not in opts.exclude):
                 feeders[f].add(s, dict(feeds.items(s)))
@@ -68,9 +78,10 @@ def main():
     try:
         out.connect()
     except Exception as e:
-        print >>sys.stderr, 'Failed to connect to mail server: %s' % e
+        log.error('Failed to connect to mail server: %s' % e)
         return -1
 
+    log.info('Looking for updates...')
     for f in feeders:
         if feeders[f] is None:
             # We failed to load this feeder.
@@ -78,15 +89,17 @@ def main():
         if f in opts.disable or (opts.enable != [] and f not in opts.enable):
             continue
         try:
+            log.info(' Scanning %s...' % f)
             for entry in feeders[f]:
                 try:
                     out.send(entry)
                 except Exception as e:
-                    print >>sys.stderr, 'Failed to send update for %s: %s' % (entry.name, e)
+                    log.error('  Failed to send update for %s: %s' % (entry.name, e))
                     return -1
         except Exception as e:
-            print >>sys.stderr, 'Warning: feeder \'%s\' threw exception: %s' % (f, e)
+            log.warning('  Feeder \'%s\' threw exception: %s' % (f, e))
 
+        log.info('  Committing resource changes...')
         # Commit resource changes.
         respath = get_resource_path(f)
         pickler.save(respath, feeders[f].resource)
